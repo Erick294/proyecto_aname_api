676312b963e808c0fa45f93a429bf4fc5f7ae126
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2024-01-22 18:36:16 -0500

    error pull

--- src/main/java/com/aname/api/security/WebSecurityConfig.java
+++ src/main/java/com/aname/api/security/WebSecurityConfig.java
@@ -42,7 +42,6 @@ public class WebSecurityConfig {
 			auth.requestMatchers("/campeonato/disponibles", "/campeonato/{id:[0-9]+}/pruebas").hasAnyAuthority("ORG", "JUN", "ADM", "ATL");
 			auth.requestMatchers("/campeonato/**").hasAnyAuthority("ORG", "JUN", "ADM");
 			auth.requestMatchers("/competidor/inscritos/**", "/competidor/confirmarInscripcion/**", "/competidor/negarInscripcion/**").hasAnyAuthority("ORG", "JUN", "ADM");
-			auth.requestMatchers("/competidor/**").hasAnyAuthority("ORG", "JUN", "ADM", "ATL");
 			auth.anyRequest().authenticated();
 		}).sessionManagement(session -> {
 			session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
